e9b77c76363ba1f59580f9328100edf6749955a3
Aaron Fanick <fanick@uwindsor.ca>
2020-08-05 23:44:12 -0400

    Import packages and images

---
+++ convolutional_neural_network.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+URL = 'https://cdn.freecodecamp.org/project-data/cats-and-dogs/cats_and_dogs.zip'
+
+path_to_zip = tf.keras.utils.get_file('cats_and_dogs.zip', origin=URL, extract=True)
+
+PATH = os.path.join(os.path.dirname(path_to_zip), 'cats_and_dogs')
+
+train_dir = os.path.join(PATH, 'train')
+validation_dir = os.path.join(PATH, 'validation')
+test_dir = os.path.join(PATH, 'test')
+
+total_train = sum([len(files) for r, d, files in os.walk(train_dir)])
+total_val = sum([len(files) for r, d, files in os.walk(validation_dir)])
+total_test = len(os.listdir(test_dir))
+
+batch_size = 128
+epochs = 15
+IMG_HEIGHT = 150
+IMG_WEIGHT = 150
